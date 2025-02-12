f579b82f0e26d52e6c94e372bc03b3955a693fcd
Eljhon Khyle Balderama <eljhonbalderama@gmail.com>
2025-02-12 21:54:58 +0800

    fixed: hero-about

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,8 +9,9 @@ const Hero = () => {
         <h1>Find Your Ideal Home Today!</h1>
         <p className='about'>
           We make finding your perfect living space easy and hassle-free. Whether you’re looking for a cozy room or a vibrant community, we’ve got you covered. 
-          With properties across the San Antonio TX area, our rooms are located in well-connected neighborhoods in Texas. Explore our properties or available rooms in person to find the perfect fit.
-        </p>
+          With properties across the San Antonio TX area. </p>
+          <p className='about2'>Our rooms are located in well-connected neighborhoods in Texas. Explore our properties or available rooms in person to find the perfect fit.</p>
+        
       </div>
       <div className='hero-image'>
         <img src={House} alt="Illustration of a modern house" />
