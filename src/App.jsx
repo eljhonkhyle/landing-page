bee004c8f063719ab027ae076f94f40c1f142b71
Eljhon Khyle Balderama <eljhonbalderama@gmail.com>
2025-02-14 03:18:14 +0800

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Carousel from './components/Carousel/Carousel';
 import Properties from './components/Properties/Properties';
 import Footer from './components/Footer/Footer';
 import Amenities from './components/Amenities/Amenities';
+import Schedule from './components/Schedule/Schedule';
+import Contact from './components/Contact/Contact';
 
 function Home() {
   return (
@@ -24,10 +26,11 @@ function App() {
       <Header />
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/amenities" element={<Amenities />} />
-        <Route path="/schedule" element={<h2>Schedule Section</h2>} />
-        <Route path="/contact" element={<h2>Contact Section</h2>} />
+        <Route path="/schedule" element={<Schedule/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
 
       <Footer />
