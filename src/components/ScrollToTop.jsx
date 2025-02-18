e283597853443e1fe9678e530c2c9476643ad0b3
Eljhon Khyle Balderama <eljhonbalderama@gmail.com>
2025-02-19 06:19:34 +0800

    fix: scroll to top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,8 +5,16 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // Scroll to the top when navigating to a new page
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  // Force scroll to top on full page reload
+  useEffect(() => {
+    window.onload = () => {
+      window.scrollTo(0, 0);
+    };
+  }, []);
 
   return null;
 };
